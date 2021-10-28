e756f810219f18700b89872ccb600fb4d8b33fb4
sea23 <sadovchenko.ea@edu.spbstu.ru>
2021-10-28 20:09:08 +0300

    chill

---
+++ cmake-build-debug/Homework1/CMakeFiles/Katya2.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/hw1/Katya2/Homework1/HWfunctions.cpp" "C:/hw1/Katya2/cmake-build-debug/Homework1/CMakeFiles/Katya2.dir/HWfunctions.cpp.obj"
+  "C:/hw1/Katya2/Homework1/mainHW.cpp" "C:/hw1/Katya2/cmake-build-debug/Homework1/CMakeFiles/Katya2.dir/mainHW.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
