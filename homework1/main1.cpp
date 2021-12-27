a1b5d43bd653065ea9bf14f91ede325be0e8e0b3
EkaterinaSad <91383753+EkaterinaSad@users.noreply.github.com>
2021-12-27 21:41:46 +0300

    Update main1.cpp

--- homework1/main1.cpp
+++ homework1/main1.cpp
@@ -9,19 +9,16 @@ int main()
 {
     double x0, y0; // базовый вектор (1й в списке)
     double *x, *y;
-   // string line;
     ifstream infile=ifstream();
     ofstream outfile = ofstream();
     infile.open("in.txt", ios::in);
     infile >> x0;
     infile >> y0;
-    //bool finished = false;
-    //cout << "x0=" << x0 << " y0=" << y0<<endl;
     int N = 0;
     double angl = -0, angr = 0, xl = 0, xr = 0, yl = 0, yr = 0;
     double norm0 = sqrt(x0 * x0 + y0 * y0);
     outfile << x0 << " " << y0 << endl;
-    while (/*!finished &&*/ !infile.eof()) { // посчитаем к-во пар чисел оставшихся
+    while (!infile.eof()) { // посчитаем к-во пар чисел оставшихся
         double x, y;
         infile >> x;
         if (infile.eof()) break;
@@ -30,8 +27,7 @@ int main()
         double norm1 = sqrt(x * x + y * y);
         double cos_val = dotpr / (norm0 * norm1);
         double z_val = x0 * y - y0 * x; // из векторного произведения
-        /* от точности вычислений зависит правильный ответ */
-        float ang = acos(cos_val) * 180.0 / PI; // "правильный" ответ
+        float ang = acos(cos_val) * 180.0 / PI; 
         if (z_val <= 0) ang = -ang;
  
         if ((angr - ang)>=0.0) {
@@ -52,6 +48,3 @@ int main()
     cout << "Rightmost: " <<  xr << " " << yr << "\n";
     cout << endl;
 }
-
-
-
